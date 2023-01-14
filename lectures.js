d5befec21084462e25e6c44ef8cc23e00c4b4336
Sara Rados <sararados@hotmail.com>
2023-01-14 01:06:31 +0100

    codechallenge1 done

---
+++ lectures.js
@@ -0,0 +1,40 @@
+
+// lecture: values and variables
+
+const country = 'Sweden';
+const continent = 'Europe';
+let population = 8000000 ;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Lecture: Data types
+
+const isIsland = 'false';
+let language;
+
+console.log( typeof isIsland);
+console.log( typeof population);
+console.log( typeof country);
+console.log( typeof language);
+
+//Lecture: const,let and var
+
+language = 'swedish';
+// Ändrat let i första lecturen till const
+
+
+//Lecture: Basic Operators
+
+let countrySplitInHalf = population / 2;
+countrySplitInHalf++;
+console.log(countrySplitInHalf);
+
+const finlandPopulation = 6000000;
+const morePeopleMyCountry = population > finlandPopulation;
+const averagePopulation = 3300000;
+const averageCountry = averagePopulation > population;
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese'
+ 
+
---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+const birthYear = 1976;
+//birthYear = 1980;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Rados';
+console.log(lastName);
+
+
+const now = 2037;
+const ageSara = now - 1976;
+const ageJesper = now - 1974;
+console.log(ageSara, ageJesper);
+
+console.log(ageSara *2, ageJesper / 10 , 2**3 );
+
+const firstName = 'Sara';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+
+let x = 10 + 5; //15
+x += 10; // x = x + 10 =25
+x *=4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--;
+x--;
+console.log(x);
+
+// comparison operators
+
+console.log(ageJesper > ageSara);  // >, < , >=, <=
+console.log(ageSara >= 18);
+
+const isFullAge = ageSara >= 18;
+
+console.log(now - 1974 > now - 1976)
+*/
+
+const now = 2037;
+const ageSara = now - 1991;
+const ageJesper = now - 2018;
+
+console.log(now - 1991 > now - 2018)
+
+console.log(25 - 10 -5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x= y = 10, x = 10; 
+console.log(x,y);
+
+const averageAge = (ageSara + ageJesper)/2
+console.log(ageSara, ageJesper, averageAge);
+
